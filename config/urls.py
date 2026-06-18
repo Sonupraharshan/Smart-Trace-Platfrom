313ab27288ddd5e107c583d0577060f4277521fd
sonu praharshan <sonupraharshan@gmail.com>
2026-06-18 21:06:05 +0530

    fix: serve media files dynamically when DEBUG=False on Render

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,16 @@
 """URL configuration for Smart Quality Inspection Platform."""
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("dashboard.urls")),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files dynamically
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
